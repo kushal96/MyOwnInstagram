f7934056b3d0e6260b665352af754d534cbcce0c
Kushal Reshamdalal <kushalreshamdalal@Kushals-MacBook-Pro.local>
2019-09-25 23:30:27 -0300

    Register

---
+++ app/src/main/java/com/example/myowninstagram/Home/HomeFragment.java
@@ -0,0 +1,25 @@
+package com.example.myowninstagram.Home;
+
+import android.os.Bundle;
+import android.text.Layout;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.myowninstagram.R;
+
+public class HomeFragment extends Fragment {
+
+    private static final String TAG = "HomeFragment";
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+
+        return view;
+    }
+}
